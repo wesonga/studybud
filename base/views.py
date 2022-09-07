1d6b1b3dea1f92ec9254a750468e7c6db7951cbf
Wesonga Bob <wesongabob1@yahoo.com>
2022-09-07 11:11:47 +0300

    Changes to the URLs and templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id:':1, 'name':'Lets learn python!'},
+    {'id:':2, 'name':'Design with me'},
+    {'id:':3, 'name':'Frontend developers'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    return render(request, 'base/room.html')
